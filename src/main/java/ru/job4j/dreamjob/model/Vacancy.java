fdfa3cbc84c42b2ef92b8f75f6ca1c6f76d5f0c8
Artem Polshchak <artem@example.com>
2024-08-08 15:14:58 +0100

    4. Thymeleaf, Циклы. [#504841 #508060]

--- src/main/java/ru/job4j/dreamjob/model/Vacancy.java
+++ src/main/java/ru/job4j/dreamjob/model/Vacancy.java
@@ -57,12 +57,15 @@ public class Vacancy {
 
     @Override
     public boolean equals(Object o) {
+
         if (this == o) {
             return true;
         }
+
         if (o == null || getClass() != o.getClass()) {
             return false;
         }
+
         Vacancy vacancy = (Vacancy) o;
         return id == vacancy.id;
     }
